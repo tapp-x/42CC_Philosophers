b3dc34285993023628feed0de238a89a8c984028
tappourc <116431821+TheoAppourchaux@users.noreply.github.com>
2024-04-08 15:27:11 +0200

    start monitoring

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: tappourc <tappourc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 10:49:22 by tappourc          #+#    #+#             */
-/*   Updated: 2024/04/06 17:47:04 by tappourc         ###   ########.fr       */
+/*   Updated: 2024/04/08 14:30:37 by tappourc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	*routine(void *phil)
 
 	philo = (t_philo *)phil;
 	ft_usleep(1);
-	// printf("enter routine with philo %d meal:%d\n", philo->id, philo->ate);
 	while(philo->ate < philo->all_data->must_eat) // fonction qui tcheck la mort en live pr tlmd
 	{
 		is_eating(philo);
@@ -31,7 +30,6 @@ void	*routine(void *phil)
 
 void	is_eating(t_philo *philo)
 {
-	// printf("enter ating\n");
 	pthread_mutex_lock(&philo->left_fork);
 	ft_safe_print("has taken a fork", philo);
 	pthread_mutex_lock(philo->right_fork);
@@ -42,4 +40,21 @@ void	is_eating(t_philo *philo)
 	ft_usleep(philo->all_data->time_to_eat);
 	pthread_mutex_unlock(philo->right_fork);
 	pthread_mutex_unlock(&philo->left_fork);
+}
+
+// dans la monitoring, check la diff entre curTime et lastmeal, si sup a t2die alors exit
+// try avec exit pr commencer
+
+void	monitoring(void *param)
+{
+	t_all	*all;
+	int		i;
+
+	all = (t_all *param);
+	i = 0;
+	while(true)
+	{
+
+	}
+
 }
